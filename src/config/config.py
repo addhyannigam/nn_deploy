f068f4a5f66f43127f2dcb925c77fe430e9491d7
addhyannigam <addhyannigam>
2024-06-02 10:41:53 +0530

    modified data_management.py and added preprocessor.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,12 +1,22 @@
+import os
+import pathlib
+import src
+
 NUM_INPUTS = 2
 NUM_LAYERS = 3
 P = [NUM_INPUTS,2,1]
-
 
 f = [None,"linear","Sigmoid"]
 
 LOSS_FUNCTION = "Mean Squared Error"
 MINI_BATCH_SIZE = 1
+
+PACKAGE_ROOT = pathlib.path(src._file_).resolve().parent
+DATAPATH = os.path.join(PACKAGE_ROOT,"datasets")
+#"/src/datasets"
+
+SAVED_MODEL_PATH = os.path.join(PACKAGE_ROOT,"trained_models")
+#"/src/trained_models"
 
 #theta0 = [None]
 #theta = [None]
