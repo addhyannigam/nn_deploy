d571a70359ff6baf494e5256f1636158f0d56576
addhyannigam <addhyannigam>
2024-06-03 16:47:56 +0530

    modified train_pipeline.py

--- src/preprocessing/data_management.py
+++ src/preprocessing/data_management.py
@@ -11,15 +11,18 @@ def load_dataset(file_name):
     #"/src/datasets/file_name"
 
     data = pd.read_csv(file_path)
-    
+
     return data
+
 
 def save_model(theta0,theta):
 
     pkl_file_path = os.path.join(config.SAVED_MODEL_PATH,"two_input_xor_nn.pkl")
 
-    with open(pkl_file_path+"/two_input_xor_nn.pkl","wb") as file_handle:
-        file_handle.dump({"params":{"biases":theta0,"weights":theta},"activation":config.f})
+    with open(pkl_file_path,"wb") as file_handle:
+        pickle.dump({"params":{"biases":theta0,"weights":theta},"activations":config.f}, file_handle)
+
+    print("Saved model with file name {} at {}".format("two_input_xor_nn.pkl",config.SAVED_MODEL_PATH))
 
 
 def load_model(file_name):
@@ -27,7 +30,6 @@ def load_model(file_name):
     pkl_file_path = os.path.join(config.SAVED_MODEL_PATH,file_name)
 
     with open(pkl_file_path,"rb") as file_handle:
-
-       loaded_model = file_handle.load()
+        loaded_model = pickle.load(file_handle)
 
     return loaded_model
